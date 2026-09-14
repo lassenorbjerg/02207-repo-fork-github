d8d9e3add3b846489a218ac560bcafd29fb45c67
norbjerg <lassenorbjerg@hotmail.com>
2026-09-14 11:59:56 +0200

    input

--- lectures/lecture03/exercises/E04_sat_cocotb_test/tb/sat_default_test.py
+++ lectures/lecture03/exercises/E04_sat_cocotb_test/tb/sat_default_test.py
@@ -6,3 +6,40 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles, RisingEdge, ReadOnly
 
+DATA_W = 8
+THRESHOLD = 64
+
+async def transaction(dut, data):
+
+    dut.in_data.value = data
+    dut.in_valid.value = 1
+
+    await RisingEdge(dut.clk)
+
+    assert dut.out_valid.value == 1
+    assert dut.out_data.value == data if data < THRESHOLD else THRESHOLD
+
+    dut.in_data.value = 0
+    dut.in_valid.value = 0
+
+    await RisingEdge(dut.clk)
+
+
+@cocotb.test()
+async def simple_test(dut):
+    Clock(signal=dut.clk, period=2, unit="ns").start()
+
+    dut.rst.value = 1
+    dut.in_valid.value = 0
+    dut.in_data.value = 0
+
+    await RisingEdge(dut.clk)
+
+    dut.rst.value = 0
+
+    await RisingEdge(dut.clk)
+
+    await transaction(dut, THRESHOLD - 5)
+
+    await transaction(dut, THRESHOLD + 5)
+
--- lectures/lecture03/exercises/E05_sat_pyuvm_test/tb/sat_default_test.py
+++ lectures/lecture03/exercises/E05_sat_pyuvm_test/tb/sat_default_test.py
@@ -8,3 +8,10 @@ from cocotb.triggers import ClockCycles, RisingEdge, ReadOnly
 import pyuvm
 from pyuvm import uvm_test
 
+@pyuvm.test()
+class sat_default_test(uvm_test):
+
+    async def build_phase(dut):
+        ...
+
+    async def connect_phase()
