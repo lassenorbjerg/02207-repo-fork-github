7bfe2432add2be8eea56fba694c5a0b53bfc36d9
lasse norbjerg <lassenorbjerg@hotmail.com>
2026-09-17 18:40:01 +0200

    E05
    does it work?

--- lectures/lecture03/exercises/E05_sat_pyuvm_test/tb/sat_default_test.py
+++ lectures/lecture03/exercises/E05_sat_pyuvm_test/tb/sat_default_test.py
@@ -8,10 +8,53 @@ from cocotb.triggers import ClockCycles, RisingEdge, ReadOnly
 import pyuvm
 from pyuvm import uvm_test
 
+DATA_W = 8
+THRESHOLD = 64
+
+
+async def transaction(dut, data):
+    dut.in_data.value = data
+    dut.in_valid.value = 1
+
+    await RisingEdge(dut.clk)
+
+    assert dut.out_valid.value == 1
+    assert dut.out_data.value == data if data < THRESHOLD else THRESHOLD
+
+    dut.in_data.value = 0
+    dut.in_valid.value = 0
+
+    await RisingEdge(dut.clk)
+
+
 @pyuvm.test()
 class sat_default_test(uvm_test):
-
-    async def build_phase(dut):
+    def build_phase(self):
+        super().build_phase()
         ...
 
-    async def connect_phase()
+    def connect_phase(self): ...
+
+    async def run_phase(self):
+        self.raise_objection()
+        super().run_phase()
+
+        dut = self.dut
+
+        Clock(signal=dut.clk, period=2, unit="ns").start()
+
+        dut.rst.value = 1
+        dut.in_valid.value = 0
+        dut.in_data.value = 0
+
+        await RisingEdge(dut.clk)
+
+        dut.rst.value = 0
+
+        await RisingEdge(dut.clk)
+
+        await transaction(dut, THRESHOLD - 5)
+
+        await transaction(dut, THRESHOLD + 5)
+
+        self.drop_objection()
