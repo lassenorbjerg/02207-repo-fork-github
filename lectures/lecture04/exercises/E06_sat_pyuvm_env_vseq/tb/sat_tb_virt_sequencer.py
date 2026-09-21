76ebcccc9a85fd66a5a9abb3dcc64fccecc1ef27
lasse norbjerg <lassenorbjerg@hotmail.com>
2026-09-21 09:48:24 +0200

    commit

--- lectures/lecture04/exercises/E06_sat_pyuvm_env_vseq/tb/sat_tb_virt_sequencer.py
+++ lectures/lecture04/exercises/E06_sat_pyuvm_env_vseq/tb/sat_tb_virt_sequencer.py
@@ -4,3 +4,15 @@
 from pyuvm import ConfigDB, uvm_sequencer
 
 
+class sat_tb_virt_sequencer(uvm_sequencer):
+    def __init__(self, name="sat_virtual_seqer", parent=None):
+        super().__init__(name, parent)
+        self.cfg = None
+        self.virtual_sequencer = None
+
+
+    def build_phase(self):
+        super().build_phase()
+
+        self.virtual_sequencer = vseqr
+
