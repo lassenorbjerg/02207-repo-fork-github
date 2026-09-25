86b6efd3906cbc1a325adcbdbcb76d4036cca45e
norbjerg <lassenorbjerg@hotmail.com>
2026-09-25 22:38:25 +0000

    passing random test

--- lectures/lecture04/exercises/E06_sat_pyuvm_env_vseq/tb/sat_random_test.py
+++ lectures/lecture04/exercises/E06_sat_pyuvm_env_vseq/tb/sat_random_test.py
@@ -6,8 +6,15 @@ import pyuvm
 from pyuvm import uvm_factory
 
 from sat_default_test import sat_default_test
+from sat_tb_defaut_seq import sat_tb_default_seq
+from sat_tb_random_seq import sat_tb_random_seq
 
-
+@pyuvm.test()
 class sat_random_test(sat_default_test):
     def start_of_simulation_phase(self):
         super().start_of_simulation_phase()
+
+        uvm_factory().set_type_override_by_type(
+            original_type=sat_tb_default_seq,
+            override_type=sat_tb_random_seq,
+        )
