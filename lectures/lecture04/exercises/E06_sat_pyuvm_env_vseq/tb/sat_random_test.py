92d6e2203be667f9a448319a80291737de5d91b9
lasse norbjerg <lassenorbjerg@hotmail.com>
2026-09-26 00:32:01 +0200

    make random test

--- lectures/lecture04/exercises/E06_sat_pyuvm_env_vseq/tb/sat_random_test.py
+++ lectures/lecture04/exercises/E06_sat_pyuvm_env_vseq/tb/sat_random_test.py
@@ -7,3 +7,7 @@ from pyuvm import uvm_factory
 
 from sat_default_test import sat_default_test
 
+
+class sat_random_test(sat_default_test):
+    def start_of_simulation_phase(self):
+        super().start_of_simulation_phase()
--- lectures/lecture04/exercises/E06_sat_pyuvm_env_vseq/tb/sat_tb_random_seq.py
+++ lectures/lecture04/exercises/E06_sat_pyuvm_env_vseq/tb/sat_tb_random_seq.py
@@ -1,4 +1,13 @@
 import random
 
 from cocotb.triggers import ClockCycles, ReadOnly, RisingEdge
+from sat_tb_defaut_seq import sat_tb_default_seq, THRESHOLD
+
+class sat_tb_random_seq(sat_tb_default_seq):
+    def __init__(self, name="sat_tb_random_seq"):
+        super().__init__(name)
+
+    async def body(self):
+        for _ in range(10):
+            await self._transaction_helper(random.randint(0,THRESHOLD*2))
 
